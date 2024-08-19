c355b63a374ffd5e5f5805944e4df66199a481db
Duke Zhu <lz320@students.waikato.ac.nz>
2024-08-20 10:49:46 +1200

    3 buttons completed

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
                 {/*<img src={logo} className="App-logo" alt="logo" />*/}
                 <h1>Recognizing</h1>
                 <div className="button-container">
+                    // 3 buttons
                     <button onClick={() => alert('button for audio recognizing')}>Audio Recognizing</button>
                     <button onClick={() => alert('button for video recognizing')}>Video Recognizing</button>
                     <button onClick={() => alert('button for image recognizing')}>Image Recognizing</button>
