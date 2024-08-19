fb4802776a558eae0034dbd49a665954727c24fc
Duke Zhu <lz320@students.waikato.ac.nz>
2024-08-20 10:49:17 +1200

    3 buttons completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                {/*<img src={logo} className="App-logo" alt="logo" />*/}
+                <h1>Recognizing</h1>
+                <div className="button-container">
+                    <button onClick={() => alert('button for audio recognizing')}>Audio Recognizing</button>
+                    <button onClick={() => alert('button for video recognizing')}>Video Recognizing</button>
+                    <button onClick={() => alert('button for image recognizing')}>Image Recognizing</button>
+                </div>
+            </header>
+        </div>
+    );
 }
+
 
 export default App;
